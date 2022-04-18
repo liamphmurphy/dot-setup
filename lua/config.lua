a5ac34721c1ed0e0572f2580fd6088c2463a9d13
Liam Murphy <liam@phmurphy.com>
2022-04-17 18:47:19 -0700

    tweaks

--- lua/config.lua
+++ lua/config.lua
@@ -1,10 +1,9 @@
 vim.g.mapleader = ";"
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
--- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
 vim.keymap.set("n", "<leader>gc", vim.lsp.buf.code_action, {buffer=0})
-vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer=0})
-vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer=0})
-vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {buffer=0})
+vim.keymap.set("n", "<leader>K", vim.lsp.buf.hover, {buffer=0})
+vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {buffer=0})
+vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, {buffer=0})
 require('lspconfig')['gopls'].setup {
     on_attach = function()
     	vim.keymap.set("n", "ga", "<cmd>GoAlternate<cr>", {buffer=0})
