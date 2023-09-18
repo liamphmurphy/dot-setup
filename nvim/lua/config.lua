6b82d09c46c60ed6c85b3198875c4384b1989b0b
Liam Murphy <liam@phmurphy.com>
2023-09-17 18:58:14 -0700

    focus more in global lsp stuff then just gopls specific

--- nvim/lua/config.lua
+++ nvim/lua/config.lua
@@ -1,21 +1,45 @@
 vim.g.mapleader = ";"
 local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 vim.keymap.set("n", "<leader>gc", vim.lsp.buf.code_action, {buffer=0})
-vim.keymap.set("n", "<leader>K", vim.lsp.buf.hover, {buffer=0})
-vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {buffer=0})
-vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, {buffer=0})
-require('lspconfig')['gopls'].setup {
-    on_attach = function()
-    	vim.keymap.set("n", "ga", "<cmd>GoAlternate<cr>", {buffer=0})
-        vim.keymap.set("n", "dl", "<cmd>Telescope diagnostics<cr>", {buffer=0})
-        vim.keymap.set("n", "go", "<cmd>GoDoc<cr>", {buffer=0})
-        vim.keymap.set("n", "ge", "<cmd>GoIfErr<cr>", {buffer=0})
-        vim.keymap.set("n", "gi", "<cmd>GoImplements<cr>", {buffer=0})
-        vim.keymap.set("n", "ds", "<cmd>GoDebugStart<cr>", {buffer=0})
-        vim.keymap.set("n", "dt", "<cmd>GoDebugStop<cr>", {buffer=0})
-        --vim.keymap.set("n", "db", "<cmd>GoDebugBreakpoint<cr>", {buffer=0})
-        vim.keymap.set("n", "dc", "<cmd>GoDebugContinue<cr>", {buffer=0})
-    end,
-    capabilities = capabilities
-}
+-- Setup language servers.
+local lspconfig = require('lspconfig')
+lspconfig.pyright.setup {}
+lspconfig.gopls.setup {}
 
+-- Global mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf }
+    vim.keymap.set('n', '<leader>gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', '<leader>gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wl', function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts)
+    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<space>f', function()
+      vim.lsp.buf.format { async = true }
+    end, opts)
+  end,
+})
